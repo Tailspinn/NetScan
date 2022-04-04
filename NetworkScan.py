7fafac440210d73ea3773652a94a4a47b393ac77
John Mora <tailspin@gmail.com>
2022-04-04 16:31:41 -0500

    Added parameters for ranges and single hosts, improved ports handling, added module support

--- NetworkScan.py
+++ NetworkScan.py
@@ -1,16 +1,22 @@
 """Scans network default or provided network for default or provided TCP ports
-usage: NetworkScan.py [-h] [-b <IP>] [-p <ports>] [-t ##]
+usage: NetworkScan.py [-h] [-b <IP>] [-p <ports>] [-m] [-t ##] [-s <IP>] [-r <IP-IP>]
 
 options:
   -h, --help            show this help message and exit
   -b <IP>, --bind <IP>  Specify IP interface to bind
   -p <ports>, --ports <ports>
                         Specify ports to scan default is -p "22,80,81,443,3389,8080"
-  -t ##, --threads ##   Specify number of threads. Default is 30 
+  -m, --module          Run as a module, supresses output, returns a list object.
+  -t ##, --threads ##   Specify number of threads. Default is 30
+  -s <IP>, --single <IP>
+                        Specify a single host to scan
+  -r <IP-IP>, --range <IP-IP>
+                        Specifies an IP range to scan
+
 
     All command line parameters are optional.
-    When called without arguments will attempt to identify the default interface ip
-    and scan it's entire subnet for the default ports TCP 22,80,81,443,3389,8080    
+    When called without arguments will dentify the default interface ip
+    and scan the entire subnet for the default ports TCP 22,80,81,443,3389,8080    
 """ 
 import ipaddress
 import socket
@@ -30,7 +36,7 @@ def get_local_ip():
     return (s.getsockname()[0])
 
 def get_hosts(net):
-    """get_hosts() will return all the hosts within a network"""
+    """get_hosts() will return all the hosts within a provided network, range"""
     network = ipaddress.IPv4Network(net)
     hosts_obj = network.hosts()
     hosts = []
@@ -58,77 +64,117 @@ def threader():
         q.task_done()
 
 def portscan(target,ports):
-    #worker function to scan a specific target
+    #worker function to scan a specific hosts ports
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #s.settimeout(1) 
-    #print('Scanning target', target)
+    s.settimeout(1) 
+    #print('DBG Scanning target', target, ports )
     openports = [] 
     mac = getmac.get_mac_address(ip=target)
     for port in ports:
-       s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+       #print("Checking port:",target, port)
        try:
            s.connect((target, int(port)))
            s.shutdown(2)
-           mac = getmac.get_mac_address(ip=target)
-           #print ("Open port: ", target, port, mac )
+           #print ("DBG Open port: ", target, port, mac )
            openports.append(port)          
        except:
            pass
-           #print("Failed:", port)
-           #return False
     if mac:
         #Attempt to lookup oui vendor
         ouilookup=OuiLookup.OuiLookup().query(mac)
         oui=str(ouilookup).replace("'", "-").split("-")
-        #Test if maybe ouilookup worked, if not say nothing
+        #Test if ouilookup worked by len, if not say nothing
         if len(oui) >= 4 and len(oui[3]) >= 3:
-            mfg = 'Vendor: ' + str(oui[3])
+            if modulecall:
+                mfg = str(oui[3])
+            else:
+                mfg = 'Vendor: ' + str(oui[3])
         else:
            mfg = " "
-        #Test if we found openports   
-        if len(openports) >= 1:
+        #Test if we found open ports   
+        if len(openports) >= 1:        
             reports='Listening ports: ' + str(openports)
         else: 
             reports=" "
-        print(target.ljust(15), mfg, reports ) 
+        if modulecall:
+            #print("Mod dbg: ",str(target) , mac , mfg , str(openports))
+            modulereturn.append ([str(target) , mac , mfg , str(openports)]) 
+        else:
+            #output findings to console
+            print(str(target).ljust(15), mfg, reports ) 
     return()
         
 def setup ():
-    #Get command line options if any, and set defaults if not.    
+    #Get command line options if any, and set defaults.    
     parser = argparse.ArgumentParser()
     parser.add_argument('-b','--bind', type=str,metavar='<IP>' , help="Specify IP interface to bind")
     parser.add_argument('-p','--ports', type=str,metavar='<ports>' , help="Specify ports to scan default is -p \"22,80,81,443,3389,8080\"")
+    parser.add_argument("-m","--module",action="store_true",help="Run as a module, supresses output, returns a list object.")
     parser.add_argument('-t','--threads', type=int,metavar='##', help="Specify number of threads. Default is 30")
-    defaultports = [22,80,81,443,3389,8080 ]
+    parser.add_argument('-s','--single', type=str,metavar='<IP>' , help="Specify a single host to scan")
+    parser.add_argument('-r','--range', type=str,metavar='<IP-IP>' , help="Specifies an IP range to scan")
+    defaultports = [22,80,443,3389,8080 ]
     args = parser.parse_args()
     if args.bind:
-        print('NetworkScan - Bind IP provided.... ')
+    #check for ip or find default ip
+        if not args.module:
+        #check to suppress output
+            print('NetworkScan - Bind IP provided.... ')
         ip = args.bind
     else:
         ip = get_local_ip()
-        print('NetworkScan - Using default IP...')
+        if not args.module:
+            print('NetworkScan - Using default IP...')
     if args.ports:
-        ports = int(args.ports)
+        ports = []
+        splitports = (args.ports).split(",")
+        for port in splitports:
+            ports.append(int(port))
     else: 
         ports = defaultports
     if args.threads:
+    #deault theads
         threads = args.threads
     else:
         threads = 30
-    getmask = get_netmask(ip)
-    targetnetwork = ipaddress.ip_network(getmask, False)
-    targethosts=get_hosts(targetnetwork)
-    print("Target network is: " + str(targetnetwork) + " with " + str(len(targethosts)) + " hosts to scan for ports : " + str(ports))
+    modulecall = args.module
+    # hash out what hosts to scan    
+    if args.single:
+        targethosts = ([str(args.single)])
+        #print("Single, ",targethosts)
+    elif args.range:
+        tgtrange = args.range.split('-')
+        start_ip = int(ipaddress.IPv4Address(tgtrange[0]))
+        end_ip = int(ipaddress.IPv4Address(tgtrange[1]))
+        end_ip = end_ip + 1
+        targethosts =[]
+        #print("Range: ", start_ip , end_ip)
+        for ip_int in range((start_ip), (end_ip)):
+            targethosts.append(str(ipaddress.IPv4Address(ip_int)))
+        #print("debug range ", targethosts)
+    else:
+        getmask = get_netmask(ip)
+        targetnetwork = ipaddress.ip_network(getmask, False)
+        targethosts=get_hosts(targetnetwork)
+    if not modulecall and not args.single and not args.range:
+        #Print informational header
+        print("Target network is: " + str(targetnetwork) + " with " + str(len(targethosts)) + " hosts to scan for ports : " + str(ports))
     if len(targethosts) >= 100:
-        print('========== Network has ' + str(len(targethosts)) + ' hosts, this scan may take over 5 minutes to complete. ==========')
-        print(' ')
-    return(targethosts, ports, threads)
+        # slow warning if large number of hosts 
+        if not modulecall:
+            print('========== Network has ' + str(len(targethosts)) + ' hosts, this scan may take over 5 minutes to complete. ==========')
+            print(' ')
+    return(targethosts, ports, threads, modulecall, args)
     
 #Gather args and find hosts to scan.
-targethosts, ports, threads = setup()
+targethosts, ports, threads, modulecall, args = setup()
 # Create the queue and threader 
 q = Queue()
 
+if modulecall:
+    #Create a list to collect return results
+    modulereturn = []
+    
 #populate the queue
 for worker in targethosts:
     q.put(worker)
@@ -142,7 +188,12 @@ for x in range(threads):
      t.start()
 
 start = time.time()
-
 # wait until the thread terminates.
 q.join()
-print("Scan completed.")
+
+if modulecall:
+    #print("DBG Module Call Dump:")
+    #print(modulereturn)
+    pass
+else:
+     print("Scan completed.")
