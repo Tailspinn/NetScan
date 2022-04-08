7ab27f91b4da8b4bc478d669f36bd1eb747988eb
John Mora <tailspin@gmail.com>
2022-04-08 01:03:12 -0500

    Rewrite handling of parameters to kwargs format so NetworkScan.networkscan() can support the same flexable input as the command line usage

--- NetworkScan.py
+++ NetworkScan.py
@@ -1,81 +1,83 @@
-"""Scans network default or provided network for default or provided TCP ports
-usage: NetworkScan.py [-h] [-b <IP>] [-p <ports>] [-m] [-t ##] [-s <IP>] [-r <IP-IP>]
+"""Scans IP network 
+usage: rewrite.py [-h] [-b <IP>] [-p <ports>] [-m] [-t ##] [-s <IP>]usage: rewrite.py [-h] [-b <IP>] [-p <ports>] [-m] [-t ##] [-s <IP>]
+
+TCP network scanner will scan the default IP interfaces entire network when no additional options are specified
 
 options:
   -h, --help            show this help message and exit
-  -b <IP>, --bind <IP>  Specify IP interface to bind
+  -b <IP>, --bind <IP>  Specify a local IP interface to bind.
   -p <ports>, --ports <ports>
                         Specify ports to scan default is -p "22,80,81,443,3389,8080"
-  -m, --module          Run as a module, supresses output, returns a list object.
+  -m, --module          Run as a module, supresses extra output, returns a list object.
   -t ##, --threads ##   Specify number of threads. Default is 30
-  -s <IP>, --single <IP>
-                        Specify a single host to scan
-  -r <IP-IP>, --range <IP-IP>
-                        Specifies an IP range to scan
+  -s <IP>, --scantarget <IP>
+                        Specify an IP, mutiple IPs, range or network to scan examples below
 
+The scantarget can be specified as a single ip, multiple ips, a range, or a CIDR network.
 
-    All command line parameters are optional.
-    When called without arguments will dentify the default interface ip
-    and scan the entire subnet for the default ports TCP 22,80,81,443,3389,8080    
-""" 
+Use Examples:
+>Networkscan.py -s 192.168.1.1   #Scan a single IP.
+>Networkscan.py -s 192.168.1.2,192.168.1.9,10.1.1.3 -b 192.168.1.207    #Scan multiple IPs, Binds to specific local IP
+>NetworkScan.py -s 192.168.1.1-192.168.1.10 -p 443,1433      #Scan a range, for ports 443 and 1433
+>Networkscan.py -m -s 192.168.1.1/24 -t 50    #Scan an entire network with 50 threads, suppresses extra output"""
+
 import ipaddress
 import socket
 import argparse
 import threading
 import time
 from queue import Queue
-
+#3rd party modules
 import ifaddr
 import getmac
 import OuiLookup
 
 def get_local_ip():
-    """ attempts a connection to find default local ip"""
+    """ attempts a connection to find default interface ip"""
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
     return (s.getsockname()[0])
 
-def get_hosts(net):
+def get_hosts(network):
     """get_hosts() will return all the hosts within a provided network, range"""
-    network = ipaddress.IPv4Network(net)
+    network = ipaddress.IPv4Network(network, strict=False)
     hosts_obj = network.hosts()
     hosts = []
     for i in hosts_obj:
         hosts.append(str(i))
     return hosts
 
-def get_netmask(local):
-    """Loop through local network adapters to find default ip match return netmask"""
+def get_netmask(localip):
+    """Loop through local network interfaces to find default ip match and return its netmask"""
     adapters = ifaddr.get_adapters()
     for adapter in adapters:
-       if str(local) in str(adapter):
+       if str(localip) in str(adapter):
            for ip in adapter.ips:
                network = str(ip.ip) + '/' + str(ip.network_prefix)
                return (network)
 
-# The threader thread pulls an worker from the queue and processes it
-def threader():
+def threader(ports,modulecall):
+    """pulls an worker from the queue and processes it"""
     while True:
         # gets an worker from the queue
         worker = q.get()
         # Run the example job with the avail worker in queue (thread)
-        portscan(worker)
+        portscan(worker,ports,modulecall)
         #completed with the job
         q.task_done()
 
-def portscan(target):
+def portscan(target,ports,modulecall):
     #worker function to scan a specific hosts ports
     #print('DBG Scanning target', target, ports )
     openports = [] 
     mac = getmac.get_mac_address(ip=target)
     for port in ports:
-       #print("Checking port:",target, port)
+    #Attempt to connect to each port
        try:
            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
            s.settimeout(1) 
            s.connect((target, int(port)))
            s.shutdown(2)
-           #print ("DBG Open port: ", target, port, mac )
            openports.append(port)          
        except:
            pass
@@ -103,75 +105,84 @@ def portscan(target):
             #output findings to console
             print(str(target).ljust(15), mfg, reports ) 
     return()
-        
-def setup ():
-    global ip, ports, threads, targethosts, modulecall
+
+
+def clisetup ():
     #Get command line options if any, and set defaults.    
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-b','--bind', type=str,metavar='<IP>' , help="Specify IP interface to bind")
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, 
+         description="TCP network scanner will scan the default IP interfaces entire network when no additional options are specified",
+         epilog="""The scantarget can be specified as a single ip, multiple ips, a range, or a CIDR network.
+\nUse Examples: 
+>Networkscan.py -s 192.168.1.1   #Scan a single IP.
+>Networkscan.py -s 192.168.1.2,192.168.1.9,10.1.1.3 -b 192.168.1.207    #Scan multiple IPs, Binds to specific local IP
+>NetworkScan.py -s 192.168.1.1-192.168.1.10 -p 443,1433      #Scan a range, for ports 443 and 1433
+>Networkscan.py -m -s 192.168.1.1/24 -t 50    #Scan an entire network with 50 threads, suppresses extra output """)
+    parser.add_argument('-b','--bind', type=str,metavar='<IP>' , help="Specify a local IP interface to bind.")
     parser.add_argument('-p','--ports', type=str,metavar='<ports>' , help="Specify ports to scan default is -p \"22,80,81,443,3389,8080\"")
-    parser.add_argument("-m","--module",action="store_true",help="Run as a module, supresses output, returns a list object.")
+    parser.add_argument("-m","--module",action="store_true",help="Run as a module, supresses extra output, returns a list object.")
     parser.add_argument('-t','--threads', type=int,metavar='##', help="Specify number of threads. Default is 30")
-    parser.add_argument('-s','--single', type=str,metavar='<IP>' , help="Specify a single host to scan")
-    parser.add_argument('-r','--range', type=str,metavar='<IP-IP>' , help="Specifies an IP range to scan")
-    defaultports = [22,80,443,3389,8080 ]
+    parser.add_argument('-s','--scantarget', type=str,metavar='<IP>' , help="Specify an IP, mutiple IPs, range or network to scan examples below")
     args = parser.parse_args()
-    if args.bind:
-    #check for ip or find default ip
-        if not args.module:
-        #check to suppress output
-            print('NetworkScan - Bind IP provided.... ')
-        ip = args.bind
+    return(args)
+    
+def networkscan(**kwargs):
+    if __name__ == "__main__":
+       #If run directly, check for command line args convert to a dict
+       clivars = vars(clisetup())
+       #Removes any values set to 'None' so kwargs.get works properly
+       kwargs = {k:v for k,v in clivars.items() if v is not None}
+    # kwargs.get(key[, default])
+    threads = int(kwargs.get('threads','30'))
+    modulecall = kwargs.get('module','0')                            
+    ip = kwargs.get('bind',get_local_ip())
+    #check for ports parameter
+    if kwargs.get('ports') is None:
+        getports = "22,80,443,3389,8080"
     else:
-        ip = get_local_ip()
-        if not args.module:
-            print('NetworkScan - Using default IP...')
-    if args.ports:
-        global ports 
-        ports = []
-        splitports = (args.ports).split(",")
-        for port in splitports:
-            ports.append(int(port))
-    else: 
-        ports = defaultports
-    if args.threads:
-    #deault theads
-        threads = args.threads
-    else:
-        threads = 30
-    modulecall = args.module
-    # hash out what hosts to scan    
-    if args.single:
-        targethosts = ([str(args.single)])
-        #print("Single, ",targethosts)
-    elif args.range:
-        tgtrange = args.range.split('-')
+        getports = kwargs.get('ports')
+    splitports = getports.split(",")
+    ports = []
+    for port in splitports:
+        ports.append(int(port))
+    # targethosts for for each case 1.1.1.1 , range 1.1.1.1-1.1.1.2 or network 1.1.2.1/12
+    if "/" in str(kwargs.get('scantarget')):
+        #if / assume a proper network
+        targethosts = get_hosts(kwargs.get('scantarget')) 
+    elif "-" in str(kwargs.get('scantarget')):
+        # if - assume a range 
+        targethosts =[]
+        tgtrange = kwargs.get('scantarget').split('-')
         start_ip = int(ipaddress.IPv4Address(tgtrange[0]))
         end_ip = int(ipaddress.IPv4Address(tgtrange[1]))
         end_ip = end_ip + 1
-        targethosts =[]
         #print("Range: ", start_ip , end_ip)
         for ip_int in range((start_ip), (end_ip)):
             targethosts.append(str(ipaddress.IPv4Address(ip_int)))
-        #print("debug range ", targethosts)
+    elif "," in str(kwargs.get('scantarget')):
+        #comma seperated list of hosts
+        targethosts = []
+        targets = str(kwargs.get('scantarget')).split(",")
+        for target in targets:
+            targethosts.append(target)
+    elif kwargs.get('scantarget'): 
+        #single target 
+        targethosts = ([kwargs.get('scantarget')])
     else:
+        #else deault to scan whole local network
         getmask = get_netmask(ip)
         targetnetwork = ipaddress.ip_network(getmask, False)
         targethosts = get_hosts(targetnetwork)
-    if not modulecall and not args.single and not args.range:
-        #Print informational header
-        print("Target network is: " + str(targetnetwork) + " with " + str(len(targethosts)) + " hosts to scan for ports : " + str(ports))
+        
     if len(targethosts) >= 100:
         # slow warning if large number of hosts 
         if not modulecall:
-            print('========== Network has ' + str(len(targethosts)) + ' hosts, this scan may take over 5 minutes to complete. ==========')
+            print('========== Network has ' + str(len(targethosts)) + ' hosts, this scan may take several minutes to complete. ==========')
             print(' ')
+    runscan(targethosts, ports, ip , threads, modulecall) 
     return ()
 
-def main ():
-    setup()
-    #Gather args and find hosts to scan.
-    #
+def runscan (targethosts, ports, ip , threads, modulecall):
+    #accept parameters and setup the scan run
     global q, modulereturn    
     if modulecall:
         #Create a list to collect return results
@@ -183,7 +194,7 @@ def main ():
         
     # how many threads
     for x in range(threads):
-         t = threading.Thread(target=threader)
+         t = threading.Thread(target=threader, args=(ports,modulecall))
          # classifying as a daemon, so they will die when the main dies
          t.daemon = True
          # begins, must come after daemon definition
@@ -191,14 +202,17 @@ def main ():
     start = time.time()
     # wait until the thread terminates.
     q.join()
-    
-    if modulecall:
-        #print("DBG Module Call Dump:")
-        print(modulereturn)
-        pass
+    if __name__ == "__main__":
+        if modulecall:
+            #print("DBG Module Call Dump:")
+            print(modulereturn)
+        else:
+            print("Scan completed.")
     else:
-        print("Scan completed.")
+        return(modulereturn)
 
+def main():
+        networkscan()
 #call main
 main () 
 
