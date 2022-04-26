1f242e2e9e3cceec4e368cb2cb4bb4e7b6bb6639
John Mora <tailspin@gmail.com>
2022-04-26 17:49:47 -0500

    Setup.py changes

--- setup.py
+++ setup.py
@@ -4,6 +4,4 @@ setup(name='NetworkScan',
       version='1.1',
       py_modules=['networkscan'],
       description="Cross platform threaded tcp network port scanner with module support.",
-      long_description= file: README.md
-      long_description_content_type='text/markdown'
       )
