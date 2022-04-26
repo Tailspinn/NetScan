abd33d81c0cd13dc93f1e0271fb1c8bd055fc69d
John Mora <tailspin@gmail.com>
2022-04-26 17:25:51 -0500

    Setup changes

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from distutils.core import setup
+long_description = ("README.md").read_text()
+
 setup(name='NetworkScan',
       version='1.1',
       py_modules=['networkscan'],
-      long_description="Cross platform threaded tcp network port scanner with module support.",
+      description="Cross platform threaded tcp network port scanner with module support.",
+      long_description=long_description,
+      long_description_content_type='text/markdown'
       )
